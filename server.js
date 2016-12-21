2a7de0732a05cd1339a9d61a0228da88e6ea9303
BeataJ <beata@emc2wave.com>
2016-12-20 17:23:48 -0800

    Halo bot

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 
 var restify = require('restify');
+var builder = require('botbuilder');
+
+//Setup restify sever
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+  console.log("%s listening to %s", server.name, server.url);
+});
+
+//Create bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+var bot = new builder.UniversalBot(connector);
+
+// Handle Bot Framework messages
+server.post('/api/messages', connector.listen());
+
+// Define root dialog
+bot.dialog('/', function(session){
+  session.send('Hallo World');
+})
